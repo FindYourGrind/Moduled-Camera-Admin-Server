c6acfe441da027f0f19f3d08b719c13c39b2c965
FindYourGrind <igortram@gmail.com>
2016-01-22 18:49:49 +0200

    Full configuration ligic, todo: video

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router  = express.Router();
 var fs      = require('fs');
+var io     = require('../../server');
 
 router.get('/config', function(req, res) {
-    fs.readFile('./app//lib/camera_config/config.json', function (err, data) {
+    fs.readFile('./app/lib/camera_config/config.json', function (err, data) {
         if (err) res.status(404).send(err.message);
         res.status(200).send(data);
     });
 });
+
+router.put('/config', function(req, res) {
+    fs.writeFile('./app/lib/camera_config/config.json', JSON.stringify(req.body, "", 4), function (err) {
+        if (err) res.status(404).send(err.message);
+        res.status(200).send('OK');
+    });
+    io.io.sockets.emit('set_config', JSON.stringify(req.body, "", 4));
+});
+
 
 module.exports = router;
--- public/js/services/ConfigService.js
+++ public/js/services/ConfigService.js
@@ -2,9 +2,18 @@ angular.module('ConfigService', [])
     .factory('Receive', ['$http', function($http) {
         var Receive = {};
 
-        Receive.receiveConfig = function (dataForReceiveing) {
+        Receive.receiveConfig = function () {
             return $http.get('/api/config');
         };
 
         return Receive;
+    }])
+    .factory('Update', ['$http', function($http) {
+        var Update = {};
+
+        Update.updateConfig = function (dataForUpdating) {
+            return $http.put('/api/config', dataForUpdating);
+        };
+
+        return Update;
     }]);
